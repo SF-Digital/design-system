46b7e1613a15856feb976f997a943ead3a6502ea
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-01-15 17:27:47 +0000

    chore: packages with first styles created

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,12 @@
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$primary500',
+  borderRadius: '$md',
+  padding: '$4',
+})
+
+export const App = () => {
+  return <Button>Hello world</Button>
+}
---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,26 @@
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  radii,
+  space,
+} from '@sf-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const { styled, css, keyframes, config, globalCss, theme, createTheme } =
+  createStitches({
+    themeMap: {
+      ...defaultThemeMap,
+      height: 'space',
+      width: 'space',
+    },
+    theme: {
+      colors,
+      fontSizes,
+      fontWeights,
+      lineHeights,
+      radii,
+      space,
+    },
+  })
