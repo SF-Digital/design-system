8bf8a14d6dbbe85e673e41eef3041b3b5ad36bbe
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-01-20 17:14:04 +0000

    docs: tokens have been documented

--- packages/docs/src/components/ColorsGrid.tsx
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -1,7 +1,7 @@
 import { colors } from '@sf-ui/tokens'
 import { getContrast } from 'polished'
 
-export function ColorsGrid() {
+export const ColorsGrid = () => {
   return Object.entries(colors).map(([key, color]) => {
     const contrast = getContrast(color, '#fff') < 3.5 ? '#000' : '#fff'
 
