99e407ccfd7c98644ec1587dfc30c5fc2e8c1d2a
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-01-21 19:52:37 +0000

    docs: component controllers

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -9,6 +9,13 @@ export default {
     src: 'https://avatars.githubusercontent.com/u/5659229?v=4',
     alt: 'Avatar',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -6,8 +6,31 @@ export default {
   component: Text,
 
   args: {
+    size: 'md',
     children:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus voluptate et quisquam officiis, dolor dolores! Esse vel perferendis, temporibus accusantium nihil quod illo? Magnam ex amet in, nostrum numquam quidem?',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
