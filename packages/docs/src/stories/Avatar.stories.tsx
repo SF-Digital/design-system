b80f773922befa2195cd21f8ff8cbebef048dc81
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-01-21 12:57:44 +0000

    feature: Avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@sf-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+
+  args: {
+    src: 'https://avatars.githubusercontent.com/u/5659229?v=4',
+    alt: 'Avatar',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+    alt: undefined,
+  },
+}
