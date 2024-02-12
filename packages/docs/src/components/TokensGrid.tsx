faaca5f94c8067a246d58108cb3569d0f6ecc385
mattwilson02 <mattwilsonbmx@gmail.com>
2024-02-12 09:46:36 +0000

    refactor: move token grid to inline styles

--- packages/docs/src/components/TokensGrid.tsx
+++ packages/docs/src/components/TokensGrid.tsx
@@ -5,24 +5,62 @@ interface TokensGridProps {
 
 export function TokensGrid({ tokens, hasRemValue = false }: TokensGridProps) {
   return (
-    <table className="tokens-grid">
+    <table
+      style={{
+        width: '100%',
+        fontFamily: 'sans-serif',
+        color: 'white',
+        borderCollapse: 'collapse',
+      }}
+    >
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Value</th>
-          {hasRemValue && <th>Pixels</th>}
+          <th style={{ padding: '0.75rem 1rem', textAlign: 'left' }}>Name</th>
+          <th style={{ padding: '0.75rem 1rem', textAlign: 'left' }}>Value</th>
+          {hasRemValue && (
+            <th style={{ padding: '0.75rem 1rem', textAlign: 'left' }}>
+              Pixels
+            </th>
+          )}
         </tr>
       </thead>
 
       <tbody>
-        {Object.entries(tokens).map(([key, value]) => {
+        {Object.entries(tokens).map(([key, value], index) => {
+          const isEven = index % 2 === 0
+
           return (
-            <tr key={key}>
-              <td>{key}</td>
-              <td>{value}</td>
+            <tr key={key} style={{ backgroundColor: isEven ? '#444' : '' }}>
+              <td
+                style={{
+                  padding: '0.75rem 1rem',
+                  color: '#ccc',
+                  borderTopLeftRadius: '8px',
+                  borderBottomLeftRadius: '8px',
+                }}
+              >
+                {key}
+              </td>
+              <td
+                style={{
+                  padding: '0.75rem 1rem',
+                  color: '#ccc',
+                }}
+              >
+                {value}
+              </td>
               {/** TODO: some of the values have only px but no rem, this is causing NaN in row 3 */}
               {hasRemValue && (
-                <td>{Number(value.replace('rem', '')) * 16}px</td>
+                <td
+                  style={{
+                    borderTopRightRadius: '8px',
+                    borderBottomRightRadius: '8px',
+                    padding: '0.75rem 1rem',
+                    color: '#ccc',
+                  }}
+                >
+                  {Number(value.replace('rem', '')) * 16}px
+                </td>
               )}
             </tr>
           )
