ffc2b5a03a34fcac9ade36a80c4dac150e8db673
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-02-19 09:51:15 +0000

    config: repository

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -27,7 +27,7 @@ const config: StorybookConfig = {
   },
   viteFinal: async (config, { configType }) => {
     if(configType === 'PRODUCTION') {
-      config.base = "/monorepo-design-system/"
+      config.base = "/design-system/"
     }
     return config;
   },
