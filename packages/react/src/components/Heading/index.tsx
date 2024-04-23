d970664e54b5d96d6b05bb50bc946a3674f677d2
mattwilson02 <mattwilsonbmx@gmail.com>
2024-04-23 13:37:48 +0100

    fix: heading class merge

--- packages/react/src/components/Heading/index.tsx
+++ packages/react/src/components/Heading/index.tsx
@@ -2,23 +2,28 @@ import { ComponentProps } from 'react'
 
 import { VariantProps, tv } from 'tailwind-variants'
 
-const heading = tv({
-  base: ['font-sf-digital leading-tight'],
-  variants: {
-    size: {
-      h1: 'text-sf-h1',
-      h2: 'text-sf-h2',
-      h3: 'text-sf-h3',
-      h4: 'text-sf-h4',
-      h5: 'text-sf-h5',
-      h6: 'text-sf-h6',
+const heading = tv(
+  {
+    base: ['font-sf-digital leading-tight'],
+    variants: {
+      size: {
+        h1: 'text-sf-h1',
+        h2: 'text-sf-h2',
+        h3: 'text-sf-h3',
+        h4: 'text-sf-h4',
+        h5: 'text-sf-h5',
+        h6: 'text-sf-h6',
+      },
+    },
+
+    defaultVariants: {
+      size: 'h3',
     },
   },
-
-  defaultVariants: {
-    size: 'h3',
+  {
+    twMerge: false,
   },
-})
+)
 
 export type HeadingProps = ComponentProps<'h1'> & VariantProps<typeof heading>
 
