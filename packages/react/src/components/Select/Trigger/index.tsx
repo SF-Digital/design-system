b0d538418ba45f2d57a2ee77070c861010eb6e7b
James McCullough <james.mccullough@stonehagefleming.com>
2024-11-07 15:16:06 +0000

    chore: select

--- packages/react/src/components/Select/Trigger/index.tsx
+++ packages/react/src/components/Select/Trigger/index.tsx
@@ -1,15 +1,30 @@
 import * as Select from '@radix-ui/react-select'
 import { ComponentProps } from 'react'
 import { twJoin } from 'tailwind-merge'
+import { tv, VariantProps } from 'tailwind-variants'
 
-export type SelectTriggerProps = ComponentProps<typeof Select.Trigger>
+const trigger = tv({
+  base: [
+    'flex flex-row items-center justify-between gap-2 rounded-md border border-neutral-50 bg-white px-3.5 py-2.5 text-sf-md leading-none text-neutral-900 focus:outline-none data-[placeholder]:font-sf-digital data-[placeholder]:font-thin data-[placeholder]:text-neutral-500',
+  ],
 
-export const Trigger = (props: SelectTriggerProps) => (
+  variants: {
+    color: {
+      'sf-green': 'data-[state="open"]:border-primary-green-500',
+      'succession-blue': 'data-[state="open"]:border-succession-blue-500',
+    },
+  },
+  defaultVariants: {
+    color: 'sf-green',
+  },
+})
+
+export type SelectTriggerProps = ComponentProps<typeof Select.Trigger> &
+  VariantProps<typeof trigger>
+
+export const Trigger = ({ color, ...props }: SelectTriggerProps) => (
   <Select.Trigger
     {...props}
-    className={twJoin(
-      'flex flex-row items-center justify-between gap-2 rounded-md border border-neutral-50 bg-white px-3.5 py-2.5 text-sf-md leading-none text-neutral-900 focus:outline-none data-[state="open"]:border-primary-green-500 data-[placeholder]:font-sf-digital data-[placeholder]:font-thin data-[placeholder]:text-neutral-500',
-      props?.className,
-    )}
+    className={twJoin(trigger({ color }), props?.className)}
   />
 )
