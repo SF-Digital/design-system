5f6653b39f26d57c8fb0d506e9531788a1da0f55
mattwilson02 <mattwilsonbmx@gmail.com>
2025-01-17 11:18:47 +0000

    chore: remove turbo build logs and .turbo cache

--- apps/react-native-doc/styles.tsx
+++ apps/react-native-doc/styles.tsx
@@ -1,25 +1,5 @@
 import { StyleSheet } from 'react-native'
 
-export const pageContainerStyle = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    gap: 10,
-    padding: 10,
-  },
-})
-
-export const exampleContainerStyle = StyleSheet.create({
-  container: {
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-    borderRadius: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 10,
-  },
-})
-
 export const styles = StyleSheet.create({
   pageContainer: {
     flex: 1,
