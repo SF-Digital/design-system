cff1f98cde08fbf28d556ddc9d23d6db38892053
mattwilson02 <mattwilsonbmx@gmail.com>
2025-01-22 12:35:29 +0000

    chore: add switch component

---
+++ apps/react-native-doc/src/app/(drawer)/switch.tsx
@@ -0,0 +1,90 @@
+import Markdown from '@ronradtke/react-native-markdown-display'
+import {
+  Switch as DesignSystemSwitch,
+  SwitchProps,
+} from '@sf-digital-ui/react-native'
+import { View } from 'react-native'
+import { styles } from '../../../styles'
+import { useState } from 'react'
+import RadioGroup from '@/components/RadioGroup'
+
+export default function Switch() {
+  const docs = `This story displays the \`Switch\` component, which is a React Native component. Types generated for each component are exported as \`SwitchProps\`.
+\b
+## Usage:
+\b
+\`\`\`import { Switch } from '@sf-digital-ui/react-native'\`\`\`
+\b
+## API Reference:
+\b
+### Root
+Props
+\b
+\`value: boolean\` - The unique value that associates the contnet with the trigger
+\b
+\`defaultValue: boolean\` - The default value of the switch
+\b
+\`onValueChange: (value: boolean) => void\` - The function to handle value change
+\b
+\`size: 'base' | 'lg'\` (Only for Trigger) - The size of the trigger (default: base)
+\b
+\`color: 'sf-green' | 'succession-blue' | 'neutral'\` (Only for Trigger) - The color of the trigger (default: sf-green)
+\b
+`
+
+  const [color, setColor] = useState<SwitchProps['color']>('sf-green')
+  const [size, setSize] = useState<SwitchProps['size']>('md')
+  const [disabled, setDisabled] = useState(false)
+
+  return (
+    <View style={[styles.pageContainer, { paddingBottom: 48 }]}>
+      <Markdown>{docs}</Markdown>
+      <View
+        style={{
+          flex: 1,
+          flexWrap: 'wrap',
+          flexDirection: 'row',
+          ...styles.componentExampleContainer,
+        }}
+      >
+        <RadioGroup
+          selectedValue={size as string}
+          onValueChange={(value) => setSize(value as SwitchProps['size'])}
+          options={[
+            {
+              label: 'sm',
+              value: 'sm',
+            },
+            {
+              label: 'md',
+              value: 'md',
+            },
+          ]}
+        />
+        <RadioGroup
+          selectedValue={color as string}
+          onValueChange={(value) => setColor(value as SwitchProps['color'])}
+          options={[
+            {
+              label: 'sf-green',
+              value: 'sf-green',
+            },
+            {
+              label: 'succession-blue',
+              value: 'succession-blue',
+            },
+          ]}
+        />
+        <RadioGroup
+          selectedValue={disabled ? 'true' : 'false'}
+          onValueChange={(value) => setDisabled(value === 'true')}
+          options={[
+            { label: 'true', value: 'true' },
+            { label: 'false', value: 'false' },
+          ]}
+        />
+      </View>
+      <DesignSystemSwitch disabled={disabled} color={color} size={size} />
+    </View>
+  )
+}
